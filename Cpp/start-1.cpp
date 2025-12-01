1fda5173317cf4312686e2f8903e4a30878b5888
Hakob Chalabyan <chhakob6379@gmail.com>
2025-12-01 09:17:52 +0400

    Cpp files start

---
+++ Cpp/start-1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+// int main()
+// {
+//     std::cout << "Hello, " << std::endl <<  "world!";
+//     return 0;
+// }
+
+
+// int main()
+// {
+//     cout << "I would like" << endl << "      to live"<< endl << "            and die in Paris" << endl;
+//     cout << "if there were " << endl << "      no lands"<< endl << "            --Armeniya ";
+// }
+
+
+int main()
+{
+    cout << "     (*)"<< endl;
+    cout << "   (*)O(*)" << endl;
+    cout << "  (*)(*)(*)" << endl;
+    cout << "   \\XXXXX/" << endl ;
+    cout << "    \\XXX/"  << endl;
+    cout << "     \\X/"  << endl;
+    cout  << "      V" << endl;
+}
